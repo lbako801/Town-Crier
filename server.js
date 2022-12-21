232b3da820fbf33c22da7a99cc594c6820ee9027
lbako801 <lbako98@gmail.com>
2022-12-20 18:00:45 -0700

    updated package.json to hold handlebars. Reinstalled socket.io properly and added dev dependency script for nodemon

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname,'public')));
 //Following code happens whenever a connection is made, including new users
 io.on('connection', socket => {
     console.log('New connection');
+    server.emit
 })
 
 // Setting server PORT as 3000 OR the environmental port
