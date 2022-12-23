45beaaad8a86ac3f2a6c458e3f1a52febe0e8ecb
lbako801 <lbako98@gmail.com>
2022-12-22 21:04:57 -0700

    enable JS function that appends socket messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 const messageBox = document.getElementById('message-box')
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
@@ -6,9 +7,10 @@ socket.on('post', post => {
     console.log(post);
 })
 
+//message output from server input
 socket.on('message', message => {
     console.log(message);
-    displayMsg();
+    displayMsg(message);
 })
 
 messageBox.addEventListener('submit', (e) => {
@@ -17,11 +19,21 @@ messageBox.addEventListener('submit', (e) => {
 
    //grabs message text from form on html
     const msg = e.target.elements.msg.value;
+    console.log(msg);
 
     //emits message box value to server
     socket.emit('messageBoxText', msg);
+
+    //clears form for ease of testing
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
 
-function displayMsg(message) {
+//output message to DOM
 
+function displayMsg(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML=`<p class="text">${message}</p>`;
+    document.querySelector('.chat-messages').appendChild(div);
 }
