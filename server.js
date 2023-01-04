8bf285684d6ec9fb9a3174f8949e8742b8cbaa7e
EfSoren <ef.sorensen@me.com>
2023-01-03 21:15:56 -0700

    Session base work

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const socketio = require("socket.io");
 //const { message } = require("statuses");
+const session = require("express-session");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,7 +15,12 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 //Telling server to use the static pages (handlebars) in the public folder
-
+const sess = {
+  secret: "secrettext",
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
