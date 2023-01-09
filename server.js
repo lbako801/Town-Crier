29b76fbcb85a1de3129fc994673c7ebe603c9c0a
lbako801 <lbako98@gmail.com>
2023-01-08 22:55:03 -0700

    added new sequelize sync function to connect to JAWSDB

--- server.js
+++ server.js
@@ -56,4 +56,8 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(Now listening on port ${PORT}));
+});
